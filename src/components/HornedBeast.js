ad74e61da6f4c0c20892482c11f99721f6e087da
Steve Ngo <stevehngo@hotmail.com>
2021-10-27 03:19:06 -0400

    finished lab02

---
+++ src/components/HornedBeast.js
@@ -0,0 +1,36 @@
+import { Component } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+
+export default class HornedBeast extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    });
+  }
+
+  render() {
+    return (
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={this.props.beast.image_url} alt={this.props.beast.description} />
+        <Card.Body>
+        <Card.Title>{this.props.beast.title}</Card.Title>
+        <Card.Text>
+          {this.props.beast.description}
+         </Card.Text>
+         <Button variant="primary" onClick={this.handleClick}>{'❤ '}{this.state.likes}</Button>
+        </Card.Body>
+      </Card>
+    )
+  }
+}
+
